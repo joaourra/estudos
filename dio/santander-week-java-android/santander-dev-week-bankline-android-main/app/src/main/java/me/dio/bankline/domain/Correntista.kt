0abcc661dab04751f0a50115761b35584275eaae
João Paulo Leonardi Urra <92340864+joaourra@users.noreply.github.com>
2022-05-06 15:24:12 -0300

    Delete dio/santander-week-java-android/santander-dev-week-bankline-android-main directory
    deleted the estudy santender diretory

--- dio/santander-week-java-android/santander-dev-week-bankline-android-main/app/src/main/java/me/dio/bankline/domain/Correntista.kt
+++
@@ -1,9 +0,0 @@
-package me.dio.bankline.domain
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Correntista(
-    val id: Int
-) : Parcelable
--- dio/santander-week-java-android/santander-dev-week-bankline-android-main/app/src/main/java/me/dio/bankline/domain/TipoMovimentacao.kt
+++
@@ -1,5 +0,0 @@
-package me.dio.bankline.domain
-
-enum class TipoMovimentacao {
-    RECEITA, DESPESA
-}
